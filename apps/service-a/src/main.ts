5a038d67d81f488563db009e8dec2d2952a30d73
Daniel Marin <danielarturomt@gmail.com>
2019-12-07 10:47:30 -0300

    improved the heart beat mechanism

--- apps/service-a/src/main.ts
+++ apps/service-a/src/main.ts
@@ -7,19 +7,44 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
+const SERVICE_NAME = 'Service A';
+const MAX_IDLE_TIME_IN_SECONDS = 10;
+const BEAT_STEP_IN_SECONDS = 1;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+
   app.setGlobalPrefix(globalPrefix);
+
+  let lastCallTs = Date.now();
+  app.use((req, res, next) => {
+    console.log(`New request arrived...`);
+    lastCallTs = Date.now();
+    next();
+  });
+
   const server = await app.listen(0);
   process.send({ port: server.address().port });
   console.log(
-    'Listening at http://localhost:' +
-      server.address().port +
-      '/' +
-      globalPrefix
+    `${SERVICE_NAME} running on http://localhost:${
+      server.address().port
+    }/${globalPrefix}`
   );
-  setTimeout(() => process.exit(), 60 * 1000);
+
+  setInterval(() => {
+    console.log(`${SERVICE_NAME} Beating...`);
+    console.log(
+      'Time passed since last request: ',
+      (Date.now() - lastCallTs) / 1000,
+      'seconds'
+    );
+    const idleSeconds = (Date.now() - lastCallTs) / 1000;
+    if (idleSeconds > MAX_IDLE_TIME_IN_SECONDS) {
+      console.log(`${SERVICE_NAME} has exitted`);
+      process.exit();
+    }
+  }, BEAT_STEP_IN_SECONDS * 1000);
 }
 
 bootstrap();
